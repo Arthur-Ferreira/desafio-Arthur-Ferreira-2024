2d2b1729e10f84cf00b7be64ee41e62d8302f721
Arthur Ferreira <arthurfdarosa@gmail.com>
2024-09-12 13:23:49 -0300

    Add animals class to use as a Blue print

---
+++ src/models/animal.js
@@ -0,0 +1,8 @@
+class Animal {
+  constructor(nome, tamanho, bioma, carnivoro = false) {
+    this.nome = nome;
+    this.tamanho = tamanho;
+    this.bioma = bioma;
+    this.carnivoro = carnivoro;
+  }
+}
